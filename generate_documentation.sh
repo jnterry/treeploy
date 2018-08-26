5fdd128d74c0907c0f2cc8b8bb67f02d4ccb562b
Jamie Terry <jnterry@ntlworld.com>
2018-08-26 14:57:14 +0100

    Add script and dependencies to generate documentation

---
+++ generate_documentation.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# Generates documentation for treeploy
+
+set -e
+
+pushd $(dirname "${0}") > /dev/null
+SCRIPTDIR=$(pwd -L)
+popd > /dev/null
+
+rm -rf ${SCRIPTDIR}/docs/
+
+$SCRIPTDIR/node_modules/jsdoc/jsdoc.js \
+		${SCRIPTDIR}/src/ \
+		-R ${SCRIPTDIR}/README.org \
+		-d ${SCRIPTDIR}/docs/ \
+		-t ${SCRIPTDIR}/node_modules/minami \
