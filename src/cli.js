b5f4eed675a7dbcf358c8445fc0eac1cf249c330
Jamie Terry <jnterry@ntlworld.com>
2018-08-27 17:34:21 +0100

    Reimplement option parsing without minimist library
    In order to do the multi param files well we need to know what order the arguments came in, including accross different argument names (eg, if specifing --modelfile name=thing --model name.lump=123 then the order of modelfile and model matters for correctly overriding earlier parameters with later, no library I can find supports this sort of thing nicely)"

--- src/cli.js
+++ src/cli.js
@@ -17,56 +17,43 @@ const treeploy   = require('./index.js');
 function treeploy_cli(arg_list){
 
 	/////////////////////////////////////////////////////////
-	// Parse inputs
-	let argv = parseArgs(arg_list, {
-		alias: {
-			warn      : ['v', 'verbose'],
-			debug     : [],
-			trace     : [],
-			help      : ['h'],
-			overwrite : [],
-			noroot    : [],
-		},
-	});
-	/////////////////////////////////////////////////////////
-
-
-
-	/////////////////////////////////////////////////////////
-	// Display help
-	if(argv.help){
+	// Deal with inputs
+	if(arg_list.indexOf('-h') > 0 || arg_list.indexOf('--help') > 0){
 		displayHelp();
 		return Q(0);
 	}
-	/////////////////////////////////////////////////////////
 
-
-
-	/////////////////////////////////////////////////////////
-	// Process inputs
-	let options = {
-		verbosity: 0,
-	};
-	let input_path    = argv['_'][0];
-	let output_path   = argv['_'][1];
-	let dot_vars_file = argv['_'][2];
-
-	if(input_path == null || output_path == null){
-		console.log("Usage: build_appdata.js INPUT_PATH OUTPUT_PATH [DOT_VARS_FILE]");
+	if(arg_list.length < 2){
+		printUsage();
 		return Q(1);
 	}
 
-	if(argv.verbose  ) { options.verbosity = 1; }
-	if(argv.debug    ) { options.verbosity = 2; }
-	if(argv.trace    ) { options.verbosity = 3; }
-	if(argv.overwrite) { options.overwrite = true; }
+	let input_path  = arg_list[0];
+	let output_path = arg_list[1];
+
+	if(input_path.startsWith('-') || output_path.startsWith('-')){
+		printUsage();
+		console.log("\nInput and output paths must be first two arguments - place flags after these!");
+		return Q(1);
+	}
+
+	let options = {};
+	try {
+		// cut off the input and output path, then parse remaining arguments
+		options = parseOptionalArguments(arg_list.splice(2));
+	} catch (e) {
+		console.log(e.message);
+		console.log("\n");
+		printUsage();
+		return Q(1);
+	}
 	/////////////////////////////////////////////////////////
 
 
 
 	/////////////////////////////////////////////////////////
 	// Check if we are root
-	if(!argv.noroot && process.getuid() != 0){
+	if(!options.noroot && process.getuid() != 0){
 		console.log('Not running as root, may not be able set file permissions, owners, etc');
 		let response = stdin.question('Continue? [y/N]');
 		if(!response.match('[Yy]|[Yy][Ee][Ss]')){
@@ -80,7 +67,7 @@ function treeploy_cli(arg_list){
 	/////////////////////////////////////////////////////////
 	// Check output does not exist, or if it does prompt user
 	// if they want to continue
-	if(!argv.overwrite && fs.existsSync(output_path)){
+	if(!options.overwrite && fs.existsSync(output_path)){
 		let out_stat = fs.statSync(output_path);
 
 		console.log("The output path already exists, it, or its decendents may be overwritten");
@@ -94,33 +81,13 @@ function treeploy_cli(arg_list){
 	/////////////////////////////////////////////////////////
 
 
-
-	/////////////////////////////////////////////////////////
-	// Load dot_vars_file
-	let dot_vars = null;
-	if(dot_vars_file != null){
-		if(!fs.existsSync(dot_vars_file)){
-			console.error("Specified dot vars file does not exist");
-			return Q(1);
-		}
-		if(dot_vars_file.endsWith('.yaml') || dot_vars_file.endsWith('.yaml')){
-			dot_vars = yaml.readSync(dot_vars_file);
-		} else if (dot_vars_file.endsWith('.json')){
-			dot_vars = JSON.parse(fs.readFileSync(dot_vars_file, 'utf8'));
-		} else if (dot_vars_file.endsWith('.js')){
-			dot_vars = require(path.resolve(dot_vars_file));
-		} else {
-			console.error("Specified dot vars file has unrecognised format!");
-			process.exit(1);
-		}
-		options.dot_models.it = dot_vars;
-	}
-	/////////////////////////////////////////////////////////
-
-
 	return treeploy(input_path, output_path, options).then(() => 0);
 }
 
+function printUsage(){
+	console.log("Usage: treeploy INPUT_PATH OUTPUT_PATH [DOT_VARS_FILE]");
+	console.log("       Run 'treeploy --help' for more information");
+}
 
 function displayHelp(){
 	console.log(`
@@ -129,15 +96,62 @@ Usage:
 
 Options:
 
-      --warn             Enables printing of warnings
-  -v  --verbose,--debug  Enables printing of debug messages, implies --warn
-      --trace            Enables printing of trace message, implies --debug
+  -v  --verbose          Increases verbosity level, can be specified between 0 and 3 times for:
+                           0 : errors only
+                           1 : above and warnings
+                           2 : above and info/debug messages
+                           3 : above and trace messages
 
       --overwrite        Disable CLI nag that destination path exists, always overwrites
       --noroot           Disable CLI nag that we are running as non-root, run anyway
 
   -h  --help             Display this help infomation
 	`);
+}
+
+function parseOptionalArguments(arg_list){
+	/////////////////////////////////////////////////////////
+	// Set default options
+	let options = {
+		noroot     : false,
+		verbosity  : 0,
+		overwrite  : false,
+		dot_models : {},
+	};
+	/////////////////////////////////////////////////////////
+
+	/////////////////////////////////////////////////////////
+	// Parse inputs
+
+	// split any combined single char args like -ab into -a and -b
+	arg_list = arg_list.map((x) => {
+		if(x.match('^-[a-z][a-z]+$')){
+			return x.substring(1).split('').map((char) => '-' + char);
+		} else {
+			return x;
+		}
+	});
+
+	// collapse any produced sub-arrays into flat list
+	arg_list = arg_list.reduce((a, c) => a.concat(c), []);
+
+	for(let i = 0; i < arg_list.length; ++i){
+		switch(arg_list[i]){
+			case '-v':
+			case '--verbose':
+				++options.verbosity;
+				break;
+			case '-h': // this must have been in a combined argument, eg -vh, so not be caught already
+				displayHelp();
+				return Q(0);
+			case '--noroot'    : options.noroot    = true; break;
+			case '--overwrite' : options.overwrite = true; break;
+			default:
+				throw new Error("Unexpected argument: " + arg_list[i]);
+		}
+	}
+
+	return options;
 }
 
 module.exports = treeploy_cli;
