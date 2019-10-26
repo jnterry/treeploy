9142d6c5435e44fb88fac5889e9e308d2fc046f2
Jamie Terry <jnterry@ntlworld.com>
2019-10-26 11:56:05 +0100

    Fix build_release.sh script to be compatible with newer pkg version

--- build_release.sh
+++ build_release.sh
@@ -1,5 +1,4 @@
 # Build the standalone executables
 mkdir -p exe/
-./node_modules/.bin/pkg ./bin/treeploy --out-path ./exe --targets linux-x86,linux-x64 --public
-mv ./exe/treeploy-x64 ./exe/treeploy-linux-x64
-mv ./exe/treeploy-x86 ./exe/treeploy-linux-x86
+./node_modules/.bin/pkg ./bin/treeploy --out-path ./exe --targets node12-linux-x64 --public
+mv ./exe/treeploy ./exe/treeploy-linux-x64
